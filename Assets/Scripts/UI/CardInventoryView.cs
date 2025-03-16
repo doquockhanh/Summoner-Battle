213850c1b8358c86e90ffb62dd77dc25d35d9c81
TienAnh2803 <tienanhv303@gmail.com>
2025-03-16 18:46:59 +0700

    update_card

--- Assets/Scripts/UI/CardInventoryView.cs
+++ Assets/Scripts/UI/CardInventoryView.cs
@@ -8,7 +8,8 @@ public class CardInventoryView : MonoBehaviour
     [SerializeField] private Image cardImage;
     [SerializeField] private TextMeshProUGUI nameText;
     [SerializeField] private TextMeshProUGUI descriptionText;
-    [SerializeField] private TextMeshProUGUI statsText;
+    [SerializeField] private TextMeshProUGUI statsTextMana;
+    [SerializeField] private TextMeshProUGUI statsTextDamge;
     
     public void Setup(Card card)
     {
@@ -17,8 +18,7 @@ public class CardInventoryView : MonoBehaviour
         descriptionText.text = card.description;
         
         // Hiển thị thông tin stats của card
-        statsText.text = $"Mana: {card.maxMana}\n" +
-                        $"Hồi mana/giây: {card.manaRegen}\n" +
-                        $"Thời gian hồi chiêu: {card.spawnCooldown}s";
+        statsTextMana.text = $"{card.maxMana}";
+        statsTextDamge.text =   $"{card.summonUnit.physicalDamage}";            
     }
 } 
